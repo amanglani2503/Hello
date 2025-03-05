d9231f0c2d75495a508c623389a4a04037b2ff69
amanglani2503 <ambermanglani8871@gmail.com>
2025-03-05 16:08:03 +0530

    Implemented Two-Way Data Binding for user input in App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   imgUrl = 'assets/Logo.png';
+  userName: string = '';
 
   openBridgeLabz() {
     window.open('https://www.bridgelabz.com', '_blank');
